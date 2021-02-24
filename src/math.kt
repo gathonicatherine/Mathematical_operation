ab4b6c0f47c6ef5d1000cb6d747eab5580985249
gathonicatherine <catherrinewanjiku@gmail.com>
2021-02-24 07:57:51 +0300

    mathematical_operations

---
+++ src/math.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+    val number1= 12.5
+    val number2= 6
+    var result: Double
+
+    result = number1 * number2
+    //println - prints the given message
+    println("number1 * number2 = $result")
+
+}
+
+
